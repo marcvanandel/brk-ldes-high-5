fdd05b0b5eeeee1e920c12bcb766357140ee0257
marcvanandel <marc.vanandel@kadaster.nl>
2022-06-28 14:20:04 +0200

    fs-extra instead of node:fs

--- prep-test-data/src/convertXml2Json.ts
+++ prep-test-data/src/convertXml2Json.ts
@@ -1,5 +1,5 @@
 import * as he from "he";
-import * as fs from "node:fs";
+import * as fs from "fs-extra";
 import * as path from "path";
 import * as txml from "txml";
 import { KoersEventMessage } from "./KoersEventMessage";
