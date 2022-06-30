5d0ab125c0ff4c141c379f5b3ef44330ab8582f6
Bob Scheer <bob.scheer@kadaster.nl>
2022-06-30 17:05:17 +0200

    upload to pldn

--- consumer-v1/src/eventProcessor.ts
+++ consumer-v1/src/eventProcessor.ts
@@ -5,6 +5,9 @@ import {
   State,
 } from "@treecg/actor-init-ldes-client";
 
+import Client from "@triply/triplydb";
+import * as fs from "fs-extra";
+
 export class EventProcessor {
   // load previous state here (e.g. load from a json file on disk)
   // private previousState: State = {
@@ -51,21 +54,32 @@ export class EventProcessor {
     } else {
       eventstreamSync = this.eventstreamSync;
     }
-
+    console.log(process.env['TRIPLYDB_TOKEN_PLDN'])
     // If the run takes longer than x minutes, pause the LDES Client
     const timeoutms = 3600000; // amount of milliseconds before timeout
     const timeout = setTimeout(() => this.eventstreamSync?.pause(), timeoutms);
+    const client = Client.get({ token: process.env['TRIPLYDB_TOKEN_PLDN']});
+    // use account
+    const account = await client.getAccount("high-5-ldes");
 
-    eventstreamSync.on("data", (member) => {
-      console.log(member);
+    // select dataset
+    const dataset = await account.getDataset("koers");
+
+    let counter = 1;
+    eventstreamSync.on("data",  (member) => {
+      eventstreamSync.pause();
+      const filename = `temporary-file-${counter}.jsonld`;
+      fs.writeFileSync(filename, member);
+      dataset.importFromFiles([filename]).then(()=>{eventstreamSync.resume()});
     });
 
-    eventstreamSync.on("metadata", (metadata) => {
-      if (metadata.treeMetadata)
-        // follows the TREE metadata extractor structure (https://github.com/TREEcg/tree-metadata-extraction#extracted-metadata)
-        console.log(metadata.treeMetadata);
-      console.log(metadata.url); // page from where metadata has been extracted
-    });
+
+    // eventstreamSync.on("metadata", (metadata) => {
+    //   if (metadata.treeMetadata)
+    //     // follows the TREE metadata extractor structure (https://github.com/TREEcg/tree-metadata-extraction#extracted-metadata)
+    //     console.log(metadata.treeMetadata);
+    //   console.log(metadata.url); // page from where metadata has been extracted
+    // });
 
     eventstreamSync.on("now only syncing", () => {
       // All known pages have been fetched at least once when receiving this event.
